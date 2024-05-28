7fa569fb704b76facf058cd882c1ae7225fbd75b
merouanezouaid <marwan.zouaid@gmail.com>
2024-05-28 17:12:08 +0100

    fixed the problem

--- api/index.py
+++ api/index.py
@@ -1,6 +1,116 @@
-from flask import Flask
+from flask import Flask, request, send_file
+from PIL import Image, ImageOps
+import io
+from rembg import remove
+import requests
+from io import BytesIO
+import os
+import base64
+
+
+from flask_cors import CORS
+
+
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+def data_uri_to_pil_image(data_uri):
+
+    # Remove the data URI header
+    header, encoded = data_uri.split(',', 1)
+
+    # Decode the base64 data
+    data = base64.b64decode(encoded)
+
+    # Create a BytesIO object from the decoded data
+    image_data = io.BytesIO(data)
+
+    # Open the image from the BytesIO object
+    image = Image.open(image_data)
+
+    return image
+
+
+# Function to process the image
+def process_image(image_file):
+
+    print(image_file)
+
+    os.makedirs('original', exist_ok=True)
+    os.makedirs('masked', exist_ok=True)
+
+    image = image_file.convert("RGB")
+
+    image.save('original/image.jpg', format = "jpeg")
+
+    output_path = "masked/image.png"
+
+    with open(output_path, "wb") as f:
+        input = open('original/image.jpg', 'rb').read()
+        subject = remove(input)
+        f.write(subject)
+
+    palestine_bg = "https://flagdownload.com/wp-content/uploads/Flag_of_Palestine_Flat_Round-1024x1024.png"
+    background_img = Image.open(BytesIO(requests.get(palestine_bg).content))
+
+
+    palestine_bg = "https://flagdownload.com/wp-content/uploads/Flag_of_Palestine_Flat_Round-1024x1024.png"
+    background_img = Image.open(BytesIO(requests.get(palestine_bg).content))
+
+    # Create a semi-transparent overlay
+    overlay = Image.new('RGBA', background_img.size, (0, 0, 0, 120))  # Adjust the last value (120) to control transparency
+    background_img = Image.alpha_composite(background_img.convert('RGBA'), overlay)
+
+    background_img = background_img.resize((image.width, image.height))
+    #image = image.resize((background_img.width, background_img.height))
+
+    background_img = background_img.convert("RGBA")
+
+    input_img = Image.open('original/image.jpg')
+
+    input_img = input_img.convert("RGBA")
+
+    combined_img = Image.alpha_composite(input_img, background_img)
+
+    combined_img = combined_img.convert('RGB')
+    # combined_img.save('masked/finale.jpg', format='jpeg')
+
+    foreground_img = Image.open(output_path)
+    combined_img.paste(foreground_img, (0,0), foreground_img)
+
+    combined_img = combined_img.convert('RGB')
+    # combined_img.save("masked/background_final.jpg", format="jpeg")
+
+    img_bytes = BytesIO()
+    combined_img.save(img_bytes, format='PNG')
+    img_bytes = img_bytes.getvalue()
+
+    return img_bytes
+
+@app.route('/process_image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.form:
+        return 'No image uploaded', 400
+
+    data_uri = request.form['image']
+    
+    
+    image_file = data_uri_to_pil_image(data_uri)
+
+
+    processed_image = process_image(image_file)
+
+    return send_file(
+        io.BytesIO(processed_image),
+        mimetype='image/png',
+        as_attachment=True,
+        download_name='free_palestine.png'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/page.jsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useState } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+
+export default function Home() {
+  const [imageFile, setImageFile] = useState(null);
+  const [croppedImage, setCroppedImage] = useState(null);
+  const [cropper, setCropper] = useState(null);
+
+  const [src, setSrc] = useState(''); // initial src will be empty
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setImageFile(file);
+  };
+
+  const handleCropImage = () => {
+    if (cropper) {
+      const croppedImageData = cropper.getCroppedCanvas().toDataURL();
+      setCroppedImage(croppedImageData);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (croppedImage) {
+      const formData = new FormData();
+      formData.append('image', croppedImage);
+
+      const response = await fetch('http://localhost:5328/process_image', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+
+        
+        const processedImage = await response.blob();
+
+        console.log(response)
+        console.log(processedImage)
+        const imageUrl = URL.createObjectURL(processedImage);
+        setSrc(imageUrl);
+      } else {
+        console.error('Error processing the image');
+      }
+    }
+  };
+
+  const dataURItoBlob = (dataURI) => {
+    const splitDataURI = dataURI.split(',');
+    const byteString =
+      splitDataURI[0].indexOf('base64') >= 0
+        ? atob(splitDataURI[1])
+        : decodeURI(splitDataURI[1]);
+    const mimeString = splitDataURI[0].split(':')[1].split(';')[0];
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i);
+    return new Blob([ia], { type: mimeString });
+  };
+
+  return (
+    <div>
+      <h1>Watermelon PFP Generator</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      {imageFile && (
+        <Cropper
+          src={URL.createObjectURL(imageFile)}
+          style={{ maxWidth: '500px', maxHeight: '500px' }}
+          viewMode={2}
+          guides={true}
+          autoCropArea={0.8}
+          autoCropAreaOrder={[0.5, 0.2, 0.3, 0.8]}
+          aspectRatio={1}
+          onInitialized={(instance) => setCropper(instance)}
+        />
+      )}
+      <button onClick={handleCropImage}>Crop Image</button>
+      {croppedImage && (
+        <>
+          <img src={croppedImage} alt="Cropped" />
+          <button onClick={handleUpload}>Upload</button>
+        </>
+      )}
+      {src && (
+      <img src={src} alt="Processed" />
+    )}
+    </div>
+  );
+}
